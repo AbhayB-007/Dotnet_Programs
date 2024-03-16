3f19835a89a801ce5d1845c484c22987598d9df5
Abhay <abhaybindal523@gmail.com>
2024-03-16 16:47:21 +0530

    v13

--- New_Learnings_in_Dotnet/DSA Practice Programs/Sorting.cs
+++ New_Learnings_in_Dotnet/DSA Practice Programs/Sorting.cs
@@ -11,21 +11,34 @@ namespace Practice
         public static void Main(String[] argd)
         {
             //----------------------------------------------------
-            // 1). Bubble Sort
-            int[] arr1 = { 5, 4, 1, 3, 2 };
-            for (int i = 0; i < arr1.Length - 1; i++)
-            {
-                for (int j = 0; j <= arr1.Length - 2 - i; j++)
-                {
-                    if (arr1[j] > arr1[j + 1])
-                    {
-                        int temp = arr1[j];
-                        arr1[j] = arr1[j + 1];
-                        arr1[j + 1] = temp;
-                    }
-                }
-            }
-            Console.WriteLine(string.Join(", ", arr1));
+            //// 1). Bubble Sort
+            //int[] arr1 = { 5, 4, 1, 3, 2 };
+            //int swaps = 0;
+            ////arr1 = new int[] { 1, 2, 3, 4, 5 }; //sorted array
+            //for (int i = 0; i < arr1.Length - 1; i++)
+            //{
+            //    if (i > 0 && swaps == 0)
+            //        break;
+            //    else
+            //    {
+            //        for (int j = 0; j <= arr1.Length - 2 - i; j++)
+            //        {
+            //            if (arr1[j] > arr1[j + 1])
+            //            {
+            //                int temp = arr1[j];
+            //                arr1[j] = arr1[j + 1];
+            //                arr1[j + 1] = temp;
+            //                swaps++;
+            //            }
+            //        }
+            //    }
+            //}
+            //if (swaps > 0)
+            //    Console.WriteLine("Sorted Array : " + string.Join(", ", arr1));
+            //else
+            //    Console.WriteLine("Array is already Sorted");
+            //----------------------------------------------------
+            //// 2). Bubble Sort
         }
 
 
