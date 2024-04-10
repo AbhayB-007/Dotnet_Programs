ec133dbbd0ae9446bffd9ea088dbae068ce3cac1
Abhay <you@example.com>
2024-04-10 12:00:50 +0530

    v20

---
+++ New_Learnings_in_Dotnet/DSA Practice Programs/2D_Arrays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class _2D_Arrays
+    {
+        public static void Main()
+        {            
+            //------------------------------------------------------
+            int[,] matrix = new int[3, 3];
+            //Input
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //output
+            Console.WriteLine("Output :- Matrix");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //------------------------------------------------------
+            int[][] myArray = new int[3][] { new int[3] { 1, 2, 3 }, new int[3] { 4, 5, 6 }, new int[3] { 7, 8, 9 } };
+            Console.WriteLine("Output :- myArray");
+            Console.WriteLine(string.Join("\n", myArray.Select(x => string.Join(" ", x))));
+
+
+        }
+    }
+}
--- New_Learnings_in_Dotnet/DSA Practice Programs/Sorting.cs
+++ New_Learnings_in_Dotnet/DSA Practice Programs/Sorting.cs
@@ -79,7 +79,7 @@ namespace Practice
             //                   --> Space Complexity -> Worst -> O(1)
             //----------------------------------------------------------
 
-            //int[] arr1 = { 5, 4, 1, 3, 2 };           
+            //int[] arr1 = { 5, 4, 1, 3, 2 };
             ////arr1 = new int[] { 1, 2, 3, 4, 5 }; //sorted array
             //for (int i = 1; i < arr1.Length; i++)
             //{
@@ -97,6 +97,39 @@ namespace Practice
             //}
             //Console.WriteLine($"Sorted Array : " + string.Join(", ", arr1));
 
+            //----------------------------------------------------------
+            //// 4). Count Sort --> Time Complexity -> Best -> O(n + k)
+            //                                      -> Avg -> O(n + k)
+            //                                      -> Worst -> O(n + k)
+            //                  --> Space Complexity -> Worst -> O(k)
+            //----------------------------------------------------------
+
+            //int[] arr1 = { 4, 3, 12, 1, 5, 5, 3, 9, 0 };
+            ////arr1 = new int[] { 1, 2, 3, 4, 5 }; //sorted array
+            //int largest = int.MinValue;
+            //for(int i = 0; i < arr1.Length; i++)
+            //{
+            //    largest = Math.Max(largest, arr1[i]);
+            //}
+
+            //int[] frequency = new int[largest + 1];
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    frequency[arr1[i]]++;
+            //}
+
+            ////sorting
+            //int j = 0;
+            //for (int i = 0;i < frequency.Length; i++)
+            //{
+            //    while (frequency[i] > 0)
+            //    {
+            //        arr1[j] = i;
+            //        j++;
+            //        frequency[i]--;
+            //    }
+            //}
+            //Console.WriteLine($"Sorted Array : " + string.Join(", ", arr1));
 
 
 
