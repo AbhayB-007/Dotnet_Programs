625d0e7c21ecba3c165de1a48c44ff5470089bea
Abhay <you@example.com>
2024-04-29 13:07:39 +0530

    dotnet practice project added

---
+++ New_Learnings_in_Dotnet/Dotnet Practice Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet_Practice_Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
