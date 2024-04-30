382ecbe732dca2a9587f9864f979b88a7eb48ac4
Abhay <abhaybindal523@gmail.com>
2024-04-30 08:31:03 +0530

    v22

--- New_Learnings_in_Dotnet/DSA Practice Programs/2D_Arrays.cs
+++ New_Learnings_in_Dotnet/DSA Practice Programs/2D_Arrays.cs
@@ -9,33 +9,34 @@ namespace Practice
     public class _2D_Arrays
     {
         public static void Main()
-        {            
+        {
             //------------------------------------------------------
-            int[,] matrix = new int[3, 3];
+            int[,] matrix2D = new int[3, 4];
             //Input
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrix2D.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrix2D.GetLength(1); j++)
                 {
-                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                    matrix2D[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
             //output
-            Console.WriteLine("Output :- Matrix");
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            Console.WriteLine("Output :- 2D Matrix");
+            for (int i = 0; i < matrix2D.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrix2D.GetLength(1); j++)
                 {
-                    Console.Write(matrix[i, j] + " ");
+                    Console.Write(matrix2D[i, j] + " ");
                 }
                 Console.WriteLine();
             }
 
             //------------------------------------------------------
-            int[][] myArray = new int[3][] { new int[3] { 1, 2, 3 }, new int[3] { 4, 5, 6 }, new int[3] { 7, 8, 9 } };
+            int[][] JaggedArray = new int[3][] { new int[5] { 1, 2, 3, 11, 12 }, new int[3] { 4, 5, 6 }, new int[4] { 7, 8, 9, 10 } };
             Console.WriteLine("Output :- myArray");
-            Console.WriteLine(string.Join("\n", myArray.Select(x => string.Join(" ", x))));
+            Console.WriteLine(string.Join("\n", JaggedArray.Select(x => string.Join(" ", x))));
 
+  
 
         }
     }
