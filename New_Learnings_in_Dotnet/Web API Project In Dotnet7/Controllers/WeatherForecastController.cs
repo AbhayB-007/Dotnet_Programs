471e406a85d857ac54598572b19c16bc41bee9b8
Abhay <you@example.com>
2024-08-04 14:41:14 +0530

    Removed the unwanted meta files form project

--- New_Learnings_in_Dotnet/Web API Project In Dotnet7/Controllers/WeatherForecastController.cs
+++ New_Learnings_in_Dotnet/Web API Project In Dotnet7/Controllers/WeatherForecastController.cs
@@ -26,8 +26,7 @@ namespace WebAPIProject_In_Dotnet_7.Controllers
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
         }
     }
 }
