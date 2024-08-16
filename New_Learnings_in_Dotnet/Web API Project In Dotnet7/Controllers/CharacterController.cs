e0764faffab9784f55e79c72902755ec392d0643
Abhay Bindal <abhaybindal523@gmail.com>
2024-08-16 21:09:36 +0530

    new changes in api project

---
+++ New_Learnings_in_Dotnet/Web API Project In Dotnet7/Controllers/CharacterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIProject_In_Dotnet_7.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+
+    }
+}
