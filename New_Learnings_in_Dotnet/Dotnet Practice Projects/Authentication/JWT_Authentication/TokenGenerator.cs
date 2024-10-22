c5998f442ae26fcde597443157a1c7cc75e6fb13
AbhayB-007 <abhaybindal523@gmail.com>
2024-10-22 18:22:00 +0530

    JWT Authentication

---
+++ New_Learnings_in_Dotnet/Dotnet Practice Projects/Authentication/JWT_Authentication/TokenGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_Authentication
+{
+    public class TokenGenerator
+    {
+        public string GenerateToken(Guid userId, string email)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = "ForTheLoveOfGodStoreAndLoadThisSecurely"u8.ToArray();
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new(JwtRegisteredClaimNames.Email, email)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = "http://id.dometrain.com",
+                Audience = "http://dometrain.com",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
