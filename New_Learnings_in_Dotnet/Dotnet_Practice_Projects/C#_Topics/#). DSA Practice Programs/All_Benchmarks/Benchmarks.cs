ffd8824ae4819e1ebc48545edaa76ff5468c86b1
AbhayB-007 <abhaybindal523@gmail.com>
2024-11-28 00:06:59 +0530

    benchmark file changes

---
+++ New_Learnings_in_Dotnet/Dotnet_Practice_Projects/C#_Topics/#). DSA Practice Programs/All_Benchmarks/Benchmarks.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Practice.All_Benchmarks
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+
+    }
+}
