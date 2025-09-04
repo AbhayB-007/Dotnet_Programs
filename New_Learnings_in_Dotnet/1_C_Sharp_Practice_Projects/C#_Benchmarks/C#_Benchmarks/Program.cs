389304fb1e29c9587c09ab5f24e1716d58fa3e2c
AbhayB-007 <abhaybindal523@gmail.com>
2025-09-04 11:35:41 +0530

    Clarified benchmark execution lines with comments, retaining the debug mode execution while commenting out the release mode execution for better clarity.

--- New_Learnings_in_Dotnet/1_C_Sharp_Practice_Projects/C#_Benchmarks/C#_Benchmarks/Program.cs
+++ New_Learnings_in_Dotnet/1_C_Sharp_Practice_Projects/C#_Benchmarks/C#_Benchmarks/Program.cs
@@ -1,9 +1,13 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using Microsoft.VisualBasic;
 
-// Run Benchmarks
+
+// To run benchmarks in debug mode
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
 
+// To run all the benchmarks in assembly in release mode
+// BenchmarkRunner.Run(typeof(Program).Assembly);
 
-
-
+// To run benchmarks in release mode
+// BenchmarkRunner.Run(typeof(Program));
