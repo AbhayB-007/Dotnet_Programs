47943f4800865688133d1c309599fc379a7ee434
AbhayB-007 <abhaybindal523@gmail.com>
2025-10-28 00:00:08 +0530

    prep started

---
+++ New_Learnings_in_Dotnet/4_fullstack_dotnetcore_dev_prep/1_complete_csharp_prep/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine(date.ToString("dd-MMM-yy"));
+        }
+    }
+}
