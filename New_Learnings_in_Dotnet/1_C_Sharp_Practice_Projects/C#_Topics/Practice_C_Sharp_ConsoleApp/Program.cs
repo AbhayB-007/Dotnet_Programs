3b8955f43277beb0a21fe1fa65649f2d0f623411
AbhayB-007 <abhaybindal523@gmail.com>
2026-04-16 15:11:42 +0530

    adding new C# interview questions

--- New_Learnings_in_Dotnet/1_C_Sharp_Practice_Projects/C#_Topics/Practice_C_Sharp_ConsoleApp/Program.cs
+++ New_Learnings_in_Dotnet/1_C_Sharp_Practice_Projects/C#_Topics/Practice_C_Sharp_ConsoleApp/Program.cs
@@ -1,20 +1,20 @@
 ﻿using Practice_C_Sharp_ConsoleApp.csharp_fundamentals_1;
+using System.Runtime.InteropServices;
 
 namespace Practice_C_Sharp_ConsoleApp
 {
     public class Program
     {
+        [DllImport("user32.dll")]
+        static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        public static void Print(string args) => Console.WriteLine(args);
+
         static void Main(string[] args)
         {
             // ---------------------------------------------------
             // datatypes
-            //DataTypes.AllDatatypes();
-
-
-            Box<List<string>> textBox = new Box<List<string>>();
-            textBox.Content = new List<string> { "Hello", "World", "!" };
-
-            Console.WriteLine("Contents of the text box: " + string.Join("", textBox.Content));
+            //DataTypes.AllDatatypes();                    
 
             // ---------------------------------------------------
             // working with files
@@ -22,12 +22,56 @@ namespace Practice_C_Sharp_ConsoleApp
 
             // ---------------------------------------------------
             // error handling
-            ErrorHandlingInCSharp.ErrorHandling();
+            //ErrorHandlingInCSharp.ErrorHandling();
+
+            // ---------------------------------------------------
+            // threads and tasks
+            //StartThread();
+            //StartTask();
+
+           
+        }
+
+        static void StartThread()
+        {
+            Thread thread = new Thread(Threads.DoWork);
+            thread.Start();
+            Console.WriteLine("Main thread continues...");
+        }
+
+        static void StartTask()
+        {
+            Task task = Task.Run(() => Tasks.DoWork());
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Main thread is doing work {i}");                
+            }           
+            task.Wait(); // wait for completion
+        }
+    }
+    class Threads
+    {
+        public static void DoWork()
+        {
+            Console.WriteLine("Work started on thread");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work completed");
+        }
+    }
+
+    class Tasks
+    {
+        public static void DoWork()
+        {
+            Console.WriteLine("Work started using Task");
+            Task.Delay(2000).Wait();
+            Console.WriteLine("Work completed");
         }
     }
 
     public class Box<T>
     {
         public T Content { get; set; }
+
     }
 }
